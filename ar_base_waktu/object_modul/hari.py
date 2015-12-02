2ec1afeeca632939123758d572121b40c31d7252
Andhitia Rama <andhitia.r@gmail.com>
2015-12-02 21:18:39 +0700

    Add ar_base_waktu

---
+++ ar_base_waktu/object_modul/hari.py
@@ -0,0 +1,42 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2004-2009 Tiny SPRL (<http://tiny.be>). All Rights Reserved
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+from openerp.osv import fields, osv
+
+
+class hari(osv.osv):
+    _name = 'base.hari'
+    _description = 'hari'
+    _columns = {
+        'name': fields.char(
+            string='Hari',
+            size=100,
+            required=True,
+            ),
+        'kode': fields.char(
+            string='Kode',
+            size=2,
+            required=True,
+            ),
+        'urutan': fields.integer(
+            string='Urutan',
+            required=True,
+            ),
+        }
