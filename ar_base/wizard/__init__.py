13c7f615e51159618d9d8bc4efd066202591fc9c
mike <mike@mike.(none)>
2015-12-11 12:06:57 +0700

    Adding OCA Depedencies

--- ar_base/wizard/__init__.py
+++ ar_base/wizard/__init__.py
@@ -19,6 +19,4 @@
 #
 ##############################################################################
 
-import wizard_cancel_transaction
-
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
+from . import wizard_cancel_transaction
